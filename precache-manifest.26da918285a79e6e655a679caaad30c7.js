96ad1fb9c94f8daaae58c9a07f08a342270c364c
Henry Lo <henrylo@Henrys-MBP.hitronhub.home>
2018-12-16 23:49:39 -0800

    Updates

---
+++ precache-manifest.26da918285a79e6e655a679caaad30c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0ed3372d1ff9c9585ba",
+    "url": "/redsnowflake/static/css/main.350cc3c0.chunk.css"
+  },
+  {
+    "revision": "c0ed3372d1ff9c9585ba",
+    "url": "/redsnowflake/static/js/main.c0ed3372.chunk.js"
+  },
+  {
+    "revision": "75bf0ac664d3a43a19ee",
+    "url": "/redsnowflake/static/css/1.983546c3.chunk.css"
+  },
+  {
+    "revision": "75bf0ac664d3a43a19ee",
+    "url": "/redsnowflake/static/js/1.75bf0ac6.chunk.js"
+  },
+  {
+    "revision": "3a1c4c9a3649d638a710",
+    "url": "/redsnowflake/static/js/runtime~main.3a1c4c9a.js"
+  },
+  {
+    "revision": "ac139bbd7cccf5b476c95cf7ad5df97d",
+    "url": "/redsnowflake/index.html"
+  }
+];
